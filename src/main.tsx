94ded3c67d496999711e48e13ef84417b5f82494
migueLopez13 <mlopezariza1996@gmail.com>
2024-06-17 22:21:45 +0200

    build: react setup

---
+++ src/main.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Setup!</div>,
+  },
+  {
+    path: "drug/:drugId",
+    element: <div>drug details</div>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
